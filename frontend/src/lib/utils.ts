57849653494748654de07384e81df395a5018b20
reevan dsouza <reevan.dsouza@codecraft.co.in>
2025-04-09 13:43:16 +0530

    feat: implement feature to format date with time

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -11,5 +11,8 @@ export function formatDate(dateString: string): string {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
   }).format(date);
 }
